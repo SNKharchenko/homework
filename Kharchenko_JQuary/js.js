a5dd01080611a0292fb3109d22fb4a765c24d9bb
SNKharchenko <svetaxa@gmail.com>
2016-09-24 21:08:07 +0300

    JQuary

---
+++ Kharchenko_JQuary/js.js
@@ -0,0 +1,120 @@
+﻿//калькулятор
+
+var firstNumber;
+var operation;
+var res = $("#result");
+//оживим 10 кнопок
+function addNumeral(numeral) {
+	res.val(res.val() + numeral);
+}
+var numSeven = $("#seven");
+	 numSeven.click(function () {
+		 addNumeral(7);
+});
+var numEight = $("#eight");
+	 numEight.click(function () {
+		 addNumeral(8);
+});
+var numNine = $("#nine");
+	 numNine.click(function () {
+		 addNumeral(9);
+});
+var numFour = $("#four");
+	 numFour.click(function () {
+		 addNumeral(4);
+});
+var numFive = $("#five");
+	 numFive.click(function () {
+		 addNumeral(5);
+});
+var numSix = $("#six");
+	 numSix.click(function () {
+		 addNumeral(6);
+});
+var numOne = $("#one");
+	 numOne.click(function () {
+		 addNumeral(1);
+});
+var numTwo = $("#two");
+	 numTwo.click(function () {
+		 addNumeral(2);
+});
+var numThree = $("#three");
+	 numThree.click(function () {
+		 addNumeral(3);
+});
+var numZero = $("#zero");
+	 numZero.click(function () {
+		 addNumeral(0);
+});
+	 
+//+-*/
+var act = $("#division");
+	act.click(function () {
+	firstNumber = res.val();
+	operation = "/";
+	res.val("");
+});
+var act = $("#multiply");
+	act.click(function () {
+	firstNumber = res.val();
+	operation = "*";
+	res.val("");
+});
+var act = $("#minus");
+	act.click(function () {
+	firstNumber = res.val();
+	operation = "-";
+	res.val("");
+});
+var act = $("#plus");
+	act.click(function () {
+	firstNumber = res.val();
+	operation = "+";
+	res.val("");
+});
+
+//= деление на ноль
+var butEqual = $("#equal");
+ butEqual.click(function () {
+	switch (operation) {
+		case "+":
+	      res.val(+firstNumber +  +res.val());
+		  break;
+		case "-":
+		  res.val(+firstNumber -  +res.val());
+		  break;
+		case "*":
+		  res.val(+firstNumber *  +res.val());
+		  break;
+		case "/":
+		    switch (true) {
+			  case res.val()!= 0:
+			    res.val(+firstNumber /  +res.val());
+			    break;
+			  case res.val() == 0:
+			    alert("Деление на ноль невозможнo");
+			    break;
+		  }
+		break;
+	}
+});
+//точка
+ var numPoint = $("#point");
+    numPoint.click(function () {
+   if (res.val().indexOf(".") === -1) {
+   res.val() += ".";
+   }
+  });
+ //сброс
+ var numReset = $("#reset");
+     numReset.click(function () {
+	 res.val("");
+ });
+  
+ //+/-
+ var numPlusMinus = $("#plusMinus");
+   numPlusMinus.click(function () {
+   res.val() = res.val() * -1;
+ });
+
---
+++ Kharchenko_JQuary/test.js
@@ -0,0 +1,30 @@
+﻿// тест
+
+function allQuestions() {
+   var mark = 0;
+   if($("#linkteg1").is(':checked')) {
+	mark = mark +1;
+}
+   if($("#numberedlist3").is(':checked')) {
+	mark = mark +1;
+}
+   if($("#passteg4").is(':checked')) {
+	mark = mark +1;
+}
+   if($("#tableteg2").is(':checked') && $("#tableteg4").is(':checked') && !$("#tableteg1").is(':checked') && !$("#tableteg3").is(':checked')) {
+	mark = mark +1;
+}
+
+  if($("#formteg1").is(':checked') && $("#formteg2").is(':checked') && !$("#formteg3").is(':checked') && !$("#formteg4").is(':checked')) {
+	mark = mark +1;
+}
+ alert(mark + " баллов из 5, " + "Ваш результат " + mark * 20 + "% верных ответов");
+ 
+ //сброс результата
+ 
+ $('input:checked').prop('checked', false);
+ 
+}
+$("#testBotton").bind('click', function() {allQuestions();});
+
+
